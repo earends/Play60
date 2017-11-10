51e789d13a5db6ffa4e62f1db3dd4af7ce45222d
Evan Arends <evanarends@Evans-MacBook-Air.local>
2017-11-09 20:39:38 -0800

    added a lot

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,27 +1,117 @@
 var Client = require('mariasql');
 
 module.exports = {
-	allUsers(req, res) {
+
+	home(req, res) {
 		var c = new Client({
 		  host: '127.0.0.1',
 		  user: 'root',
 		  password: '',
-		  db: 'test'
+		  db: 'play60'
 		});
 
-		var prep = c.prepare('SELECT name FROM User');
+		var find = c.prepare('SELECT * FROM Posting');
 
-		c.query(prep(), function(err, rows) {
+		c.query(find(), function(err, rows) {
 		  if (err)
 		    throw err;
-		  console.dir(rows);
+		  res.render('home', { postings: rows });
+		  
 		});
 
 		c.end();
+
 		
+	},
+	login(req, res) {
+		res.render('login', { title: 'Express' });
+	},
+	signup(req, res) {
+		res.render('signup', { title: 'Express' });
+	},
+	createPost(req, res) {
+
+		res.render('create_post', { title: 'Express' });
+	},
+	profile(req, res) {
+		res.render('profile', { title: 'Express' });
+	},
+	profile_settings(req, res) {
+		res.render('profile_settings', { title: 'Express' });
+	},
+	profile_settings_save(req, res) {
+		res.send('profile_settings_save');
+	},
+	login_post(req, res) {
+		var c = new Client({
+		  host: '127.0.0.1',
+		  user: 'root',
+		  password: '',
+		  db: 'play60'
+		});
+		
+		var check = c.prepare('SELECT UID FROM User WHERE email =' + "'" + req.body.email_input + "'" + 'AND password =' +"'" +req.body.password_input + "'");
+		//select * from User where email ='earends@zagmail.gonzaga.edu' and password = 'password'
+		c.query(check(), function(err, rows) {
+		  if (err)
+		    throw err;
+		  console.log(rows);
+		  if (rows.info.numRows == 1) {
+		  	
+		  	res.redirect('/' + rows[0].UID + '/home');
+		  }
+		});
+
+		c.end();
+	},
 
 
-		res.render('index', { title: 'Express' });
-		}
+	signup_post(req, res) {
+		var c = new Client({
+		  host: '127.0.0.1',
+		  user: 'root',
+		  password: '',
+		  db: 'play60'
+		});
+		
+		var check = c.prepare('SELECT email FROM User WHERE email =' + "'" + req.body.email_input + "'");
+		var insert = c.prepare("INSERT INTO User (name,age,email,password) VALUES (" + "'" + req.body.name_input + "'" + "," + "'" + req.body.age_input + "'" + "," + "'" + req.body.email_input + "'"+ "," + "'" + req.body.password_input + "'" + ")");
+		//checks to see if email is already in their if so re route to signup
+		c.query(check(), function(err, rows) {
+		  if (err)
+		    throw err;
+		  if (rows.info.numRows != 0) {
+		  	res.redirect('/signup');
+		  } else { // else if your all good
+			c.query(insert(), function(err, rows) {
+				res.redirect('/login');
+			});
+		  }
+		});
+		// adds email if good route to login page if no good re reoute to signup page
+		
+		
+		
+		c.end();
+		
+	},
+	createPost_save(req,res) {
+		var c = new Client({
+		  host: '127.0.0.1',
+		  user: 'root',
+		  password: '',
+		  db: 'play60'
+		});
+
+		var insert = c.prepare("INSERT INTO Posting (title,game,description,UID,event_time) VALUES (" + "'" + req.body.title_input + "'" + "," + "'" + req.body.game_input + "'"+ "," + "'" + req.body.details_input + "'"+ "," + "'" + parseInt(req.params.id) + "'"+ "," + "'" + req.body.time_input + "'" + ")" )
+		c.query(insert(), function(err, rows) {
+			if (err)
+				throw err
+		  	res.redirect('/' + req.params.id + '/home');
+		});
+
+		c.end();
+	}
+
 
 }
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,46 @@ var router = express.Router();
 var helpers = require('../controllers/homeController.js');
 
 
-module.exports = function (app) {
-	app.route('/')
-		.get(helpers.allUsers)
-		//.post(helpers.createUser);
+/* GET main page route to login page */
+router.get('/', function(req, res, next) {
+  res.redirect('/login');
+});
 
-};
+//GET login page
+router.get('/login',helpers.login);
 
+//redirect to login page because no id
+router.get('/home', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+//signup page
+router.get('/signup',helpers.signup);
+
+//get personalized home page
+router.get('/:id/home',helpers.home);
+
+//get post page
+router.get('/:id/createPost',helpers.createPost);
+
+//get user profile page
+router.get('/:id/profile',helpers.profile);
+
+//get user profile edit page
+router.get('/:id/profile_settings',helpers.profile_settings);
+
+//post user profile settings save page
+router.post('/:id/profile_settings',helpers.profile_settings_save);
+
+//post login info
+router.post('/login',helpers.login_post);
+
+//post signup info
+router.post('/signup',helpers.signup_post);
+
+//create post POST
+router.post('/:id/createPost',helpers.createPost_save);
+
+
+module.exports = router;
 
